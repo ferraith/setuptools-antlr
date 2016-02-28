7311b39494fb8f2dd6d94dc517148d07449635b7
ferraith <ferraith@gmail.com>
2016-02-28 22:04:07 +0100

    Added inital version of hello example

---
+++ example/hello/frontend/hello.py
@@ -0,0 +1,9 @@
+"""hello entry point."""
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
---
+++ example/setup.py
@@ -0,0 +1,22 @@
+"""Setup script for hello."""
+from distutils.command.build import build as _build
+from setuptools import setup, find_packages
+from antlr_distutils.build_antlr import build_antlr
+
+
+class build(_build):
+    sub_commands = _build.sub_commands + [
+        ('build_antlr', None)
+    ]
+
+if __name__ == '__main__':
+    setup(
+        name='hello',
+        version='0.1',
+        description='Example of use of ANTLR',
+        author='Andreas Schmidl',
+        author_email='Andreas.Schmidl@gmail.com',
+        packages=find_packages(),
+        license='MIT',
+        cmdclass={'build': build, 'build_antlr': build_antlr}
+    )
