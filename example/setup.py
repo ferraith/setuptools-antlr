cedb7592b1186bb39dc4bffa7572d5fdf53a97e7
ferraith <ferraith@gmail.com>
2016-12-13 19:05:48 +0100

    Fixed version schema

--- example/setup.py
+++ example/setup.py
@@ -5,7 +5,7 @@ if __name__ == '__main__':
 
     setup(
         name='foobar',
-        version='0.1',
+        version='0.0.1',
         description='Example of use of ANTLR',
         author='Andreas Schmidl',
         author_email='Andreas.Schmidl@gmail.com',
