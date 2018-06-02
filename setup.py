9bc7cd02afc7ae211057b641125bff06b95b156c
ferraith <ferraith@gmail.com>
2018-06-02 13:03:00 +0200

    ferraith#21: Bumped up version to 0.2.0

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ if __name__ == '__main__':
 
     setuptools.setup(
         name='setuptools-antlr',
-        version='0.1.2',
+        version='0.2.0',
         packages=setuptools.find_packages(),
         package_data={'setuptools_antlr': ['lib/antlr-4.7.1-complete.jar', 'lib/LICENSE.txt']},
         entry_points={
