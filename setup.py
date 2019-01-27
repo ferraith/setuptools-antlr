96f11fe4111b78d429db575156cd8e1b58a8c8ed
ferraith <ferraith@gmail.com>
2019-01-27 21:52:19 +0100

    Fixed trove classifiers

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ if __name__ == '__main__':
             'Topic :: Software Development',
             'License :: OSI Approved :: MIT License',
             'Programming Language :: Python :: 3.5',
-            'Programming Language :: Python :: 3.6'
-            'Programming Language :: Python :: 3.7'
+            'Programming Language :: Python :: 3.6',
+            'Programming Language :: Python :: 3.7',
         ]
     )
